6c7878ab32583b73764b5ecb6e5209f3c7548002
priyamani6384-wq <priyamani6384@gmail.com>
2026-09-12 11:58:48 +0530

    Create models.py

---
+++ shop/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    stock = models.IntegerField(default=100)
+    image = models.ImageField(upload_to='products/', blank=True)
+    is_available = models.BooleanField(default=True)
+    def __str__(self):
+        return self.name
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    shipping_address = models.TextField()
+    payment_status = models.CharField(max_length=20, default='Paid')
+    created_at = models.DateTimeField(auto_now_add=True)
